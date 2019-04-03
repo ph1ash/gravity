97968d8a1012e25079ccac729257f13a6ffe2db8
Matt Dresser <m.dres344@gmail.com>
2019-04-02 23:05:52 -0700

    Artwork API + Weather API up and running. Shifted Flask setup functionality to WSGI scripts

---
+++ scraper-prod.py
@@ -0,0 +1,75 @@
+from flask_restful import Resource, reqparse
+import requests
+import json
+import numpy as np
+from appId import AppId
+
+class Scraper(Resource):
+    def get(self):
+        return ValidateAppId(GetPaoUrl)
+
+    # def post(self):
+    #    args = parser.parse_args()
+
+class Weather(Resource):
+    def get(self):
+        return ValidateAppIdThenRun(GetWeather)
+
+def ValidateAppIdThenRun(functionToCall):
+    parser = reqparse.RequestParser()
+    parser.add_argument('appId')
+    args = parser.parse_args()
+    if (args['appId'] is not None) and (args['appId'] == AppId.id):
+        return functionToCall()
+    else:
+        return {"message": "Invalid App ID provided", "code": "invalidAppId"}
+
+def GetPaoUrl():
+    baseUrl = "https://www.artstation.com/users/pao/projects.json"
+    pageIdx = 1
+    jpgList = []
+
+    while 1:
+        page = requests.get(baseUrl + "?page=" + str(pageIdx))
+        parseJson = json.loads(page.text)
+        # If the data is empty, break out of the loop
+        if parseJson['data'] == []:
+            break
+        else: # Otherwise add the entry into the list
+            for entry in parseJson['data']:
+                jpgList.append(str(entry['cover']['thumb_url'].encode('utf-8').decode('latin-1')))
+                #jpgList.append(str(entry['cover']['micro_square_image_url'].encode('utf-8').decode('latin-1')))
+        pageIdx += 1
+    selectionValue = int(np.random.uniform(low=0, high=len(jpgList)))
+    return (jpgList[selectionValue]), 200
+
+def GetWeather():
+    baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+    appId = "ee3f3b74571c87be2297e528f4074376"
+    parser = reqparse.RequestParser()
+    lat = lon = ""
+    parser.add_argument('lat')
+    parser.add_argument('lon')
+    args = parser.parse_args()
+
+    if (args['lat'] is not None) and (args['lon'] is not None):
+        try :
+            lat = str(np.around(float(args['lat']), decimals=3))
+            lon = str(np.around(float(args['lon']), decimals=3))
+        except ValueError:
+            response = {"message": "lat or lon input is invalid", "code": "badLatLon"}
+            return response
+        except:
+            response = {"message": "Unknown error occured", "code":"weatherUnknown"}
+            return response
+        baseUrl = baseUrl + "lat=" + lat + "&lon=" + lon + "&appid=" + appId + "&units=imperial"
+        page = requests.get(baseUrl)
+        parseJson = json.loads(page.text)
+        return parseJson
+    else:
+        return "", 400
+
+
+def ScraperAddApi(api):
+    api.add_resource(Scraper, "/api/artwork")
+    api.add_resource(Weather, "/api/weather")
--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ class Url(Resource):
     def get(self):
         return GetPaoUrl(), 200
 
+#@app.route("/")
 def GetPaoUrl():
     baseUrl = "https://www.artstation.com/users/pao/projects.json"
     pageIdx = 1
@@ -28,7 +29,9 @@ def GetPaoUrl():
                 #jpgList.append(str(entry['cover']['micro_square_image_url'].encode('utf-8').decode('latin-1')))
         pageIdx += 1
     selectionValue = int(np.random.uniform(low=0, high=len(jpgList)))
-    return(jpgList[selectionValue])
+    return (jpgList[selectionValue])
 
-api.add_resource(Url, "/getPaoUrl")
-app.run(ssl_context=('/etc/letsencrypt/live/mattdresser.com/fullchain.pem','/etc/letsencrypt/live/mattdresser.com/privkey.pem'), host='0.0.0.0')
+if __name__ == '__main__':
+    api.add_resource(Url, "/getPaoUrl")
+    #app.run()
+    app.run(ssl_context=('/etc/letsencrypt/live/mattdresser.com/fullchain.pem','/etc/letsencrypt/live/mattdresser.com/privkey.pem'), host="0.0.0.0")
