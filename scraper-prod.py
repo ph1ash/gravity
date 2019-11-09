f7a9863004fb9a7122e9a24c3ae4508ca86e7827
Matt Dresser <m.dres344@gmail.com>
2019-11-08 17:28:39 -0800

    Shifted things around to maintain global jpgList. This way we don't have to make requests over and over. The server will probably restart more frequently than the amount of new artwork that would come up if we manually updated.

--- scraper-prod.py
+++ scraper-prod.py
@@ -3,73 +3,85 @@ import requests
 import json
 import numpy as np
 from appId import AppId
+from fake_useragent import UserAgent
+from datetime import date
+
+ua = UserAgent()
+ua.random
 
 class Scraper(Resource):
-    def get(self):
-        return ValidateAppIdThenRun(GetPaoUrl)
-
-    # def post(self):
-    #    args = parser.parse_args()
-
-class Weather(Resource):
-    def get(self):
-        return ValidateAppIdThenRun(GetWeather)
-
-def ValidateAppIdThenRun(functionToCall):
-    parser = reqparse.RequestParser()
-    parser.add_argument('appId')
-    args = parser.parse_args()
-    if (args['appId'] is not None) and (args['appId'] == AppId.id):
-        return functionToCall()
-    else:
-        return {"message": "Invalid App ID provided", "code": "invalidAppId"}
-
-def GetPaoUrl():
-    baseUrl = "https://www.artstation.com/users/pao/projects.json"
-    pageIdx = 1
     jpgList = []
 
-    while 1:
-        page = requests.get(baseUrl + "?page=" + str(pageIdx))
-        parseJson = json.loads(page.text)
-        # If the data is empty, break out of the loop
-        if parseJson['data'] == []:
-            break
-        else: # Otherwise add the entry into the list
-            for entry in parseJson['data']:
-                jpgList.append(str(entry['cover']['thumb_url'].encode('utf-8').decode('latin-1')))
-                #jpgList.append(str(entry['cover']['micro_square_image_url'].encode('utf-8').decode('latin-1')))
-        pageIdx += 1
-    selectionValue = int(np.random.uniform(low=0, high=len(jpgList)))
-    return (jpgList[selectionValue]), 200
+    def get(self):
+        return self.ValidateAppIdThenRun()
 
-def GetWeather():
-    baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
-    appId = AppId.weatherApiKey
-    parser = reqparse.RequestParser()
-    lat = lon = ""
-    parser.add_argument('lat')
-    parser.add_argument('lon')
-    args = parser.parse_args()
+    def GetPaoUrl(self):
+        baseUrl = "https://www.artstation.com/users/pao/projects.json"
+        pageIdx = 1
 
-    if (args['lat'] is not None) and (args['lon'] is not None):
-        try :
-            lat = str(np.around(float(args['lat']), decimals=3))
-            lon = str(np.around(float(args['lon']), decimals=3))
-        except ValueError:
-            response = {"message": "lat or lon input is invalid", "code": "badLatLon"}
-            return response
-        except:
-            response = {"message": "Unknown error occured", "code":"weatherUnknown"}
-            return response
-        baseUrl = baseUrl + "lat=" + lat + "&lon=" + lon + "&appid=" + appId + "&units=imperial"
-        page = requests.get(baseUrl)
-        parseJson = json.loads(page.text)
-        return parseJson
-    else:
-        return "", 400
+        if not self.jpgList:
+            # Fetch goodies
+            print("Fetching URLs since jpgList isn't populated")
+            while 1:
+                page = requests.get(baseUrl + "?page=" + str(pageIdx))
+                parseJson = json.loads(page.text)
+                # If the data is empty, break out of the loop
+                if parseJson['data'] == []:
+                    break
+                else: # Otherwise add the entry into the list
+                    for entry in parseJson['data']:
+                        self.jpgList.append(str(entry['cover']['thumb_url'].encode('utf-8').decode('latin-1')))
+                        #jpgList.append(str(entry['cover']['micro_square_image_url'].encode('utf-8').decode('latin-1')))
+                pageIdx += 1
+            selectionValue = int(np.random.uniform(low=0, high=len(self.jpgList)))
+            print("JPG selection made: " + self.jpgList[selectionValue])
+            return (self.jpgList[selectionValue]), 200
+        else:
+            selectionValue = int(np.random.uniform(low=0, high=len(self.jpgList)))
+            print("JPG selection made: " + self.jpgList[selectionValue])
+            return (self.jpgList[selectionValue]), 200
 
+    def ValidateAppIdThenRun(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('appId')
+        args = parser.parse_args()
+        if (args['appId'] is not None) and (args['appId'] == AppId.id):
+            return self.GetPaoUrl()
+        else:
+            return {"message": "Invalid App ID provided", "code": "invalidAppId"}
 
-def ScraperAddApi(api):
+def Initialize(api):
     api.add_resource(Scraper, "/api/artwork")
-    api.add_resource(Weather, "/api/weather")
+
+#class Weather(Resource):
+#    def get(self):
+#        return ValidateAppIdThenRun(GetWeather)
+#
+#
+#def GetWeather():
+#    baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+#    appId = AppId.weatherApiKey
+#    parser = reqparse.RequestParser()
+#    lat = lon = ""
+#    parser.add_argument('lat')
+#    parser.add_argument('lon')
+#    args = parser.parse_args()
+#
+#    if (args['lat'] is not None) and (args['lon'] is not None):
+#        try :
+#            lat = str(np.around(float(args['lat']), decimals=3))
+#            lon = str(np.around(float(args['lon']), decimals=3))
+#        except ValueError:
+#            response = {"message": "lat or lon input is invalid", "code": "badLatLon"}
+#            return response
+#        except:
+#            response = {"message": "Unknown error occured", "code":"weatherUnknown"}
+#            return response
+#        baseUrl = baseUrl + "lat=" + lat + "&lon=" + lon + "&appid=" + appId + "&units=imperial"
+#        page = requests.get(baseUrl)
+#        parseJson = json.loads(page.text)
+#        return parseJson
+#    else:
+#        return "", 400
+
+    #api.add_resource(Weather, "/api/weather")
